798d22f11ed3707597c03d493d712ba3fa35371f
Emel Alkim <emelalkim@msn.com>
2019-04-29 20:44:50 -0700

    adds test for all the defined API endpoints using no authorization. stows a study first and validates the stored values

---
+++ test/stowTest.js
@@ -0,0 +1,73 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const fs = require('fs');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+// as these are outside any describe, they are global to all tests!
+let server;
+before(async () => {
+  process.env.host = '0.0.0.0';
+  process.env.port = 5987;
+  server = require('../server'); // eslint-disable-line
+  await server.ready();
+});
+after(() => {
+  server.close();
+});
+
+describe('STOW Tests', () => {
+  it('studies should be empty', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get('/studies')
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(0);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('stow should succeed with multipart study', done => {
+    const buffer = fs.readFileSync('test/data/multipart_study');
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .post('/studies')
+      .set(
+        'content-type',
+        'multipart/related; type=application/dicom; boundary=--594b1491-fdae-4585-9b48-4d7cd999edb3'
+      )
+      .send(buffer)
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('stow should fail with dicom file', done => {
+    const buffer = fs.readFileSync('test/data/image.dcm');
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .post('/studies')
+      .set(
+        'content-type',
+        'multipart; type=application/dicom; boundary=--594b1491-fdae-4585-9b48-4d7cd999edb3'
+      )
+      .send(buffer)
+      .then(res => {
+        expect(res.statusCode).to.equal(503);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+});
---
+++ test/wadoTest.js
@@ -0,0 +1,141 @@
+require('./stowTest');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('WADO Tests', () => {
+  it('it should get dicom file for instance 1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get(
+        '/studies/1.3.6.1.4.1.14519.5.2.1.1706.4996.267501199180251031414136865313/series/1.3.6.1.4.1.14519.5.2.1.1706.4996.170872952012850866993878606126/instances/1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694'
+      )
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res).to.have.header(
+          'Content-Disposition',
+          'attachment; filename=1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694.dcm'
+        );
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('it should GET metadata of study 1.3.6.1.4.1.14519.5.2.1.1706.4996.267501199180251031414136865313', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get('/studies/1.3.6.1.4.1.14519.5.2.1.1706.4996.267501199180251031414136865313/metadata')
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(19);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('it should GET empty metadata for madeup study 1.3.6.1.4.1.65476457.5.2.1.1706.4996.6436336251031414136865313', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get('/studies/1.3.6.1.4.1.65476457.5.2.1.1706.4996.6436336251031414136865313/metadata')
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(0);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('it should GET metadata of series 1.3.6.1.4.1.14519.5.2.1.1706.4996.170872952012850866993878606126', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get(
+        '/studies/1.3.6.1.4.1.14519.5.2.1.1706.4996.267501199180251031414136865313/series/1.3.6.1.4.1.14519.5.2.1.1706.4996.170872952012850866993878606126/metadata'
+      )
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(18);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('it should GET empty metadata for madeup study study 1111111111 and series 1.3.6.1.4.1.54747.5.2.1.1706.4996.4562342246724757457', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get(
+        '/studies/1111111111/series/1.3.6.1.4.1.54747.5.2.1.1706.4996.4562342246724757457/metadata'
+      )
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(0);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('it should GET metadata of instance 1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get(
+        '/studies/1.3.6.1.4.1.14519.5.2.1.1706.4996.267501199180251031414136865313/series/1.3.6.1.4.1.14519.5.2.1.1706.4996.170872952012850866993878606126/instances/1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694/metadata'
+      )
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(1);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('the metadata of instance 1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694 should contain the uid', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get(
+        '/studies/1.3.6.1.4.1.14519.5.2.1.1706.4996.267501199180251031414136865313/series/1.3.6.1.4.1.14519.5.2.1.1706.4996.170872952012850866993878606126/instances/1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694/metadata'
+      )
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body[0]['00080018'].Value[0]).to.be.eql(
+          '1.3.6.1.4.1.14519.5.2.1.1706.4996.101091068805920483719105146694'
+        );
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+
+  it('it should GET empty metadata for madeup study study 1111111111, series 222222222222 and instance 3333333333333', done => {
+    chai
+      .request(`http://${process.env.host}:${process.env.port}`)
+      .get('/studies/1111111111/series/222222222222/instances/3333333333333/metadata')
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.eql(0);
+        done();
+      })
+      .catch(e => {
+        done(e);
+      });
+  });
+});
