df923c57c897f3e41ed2e5e3c55f93f07dc442ad
Erik Ziegler <erik.sweed@gmail.com>
2020-06-16 10:59:21 +0200

    Reformat server-config with prettier

--- dockersupport/server-config.js
+++ dockersupport/server-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    env: process.env.NODE_ENV,
-    dbServer: process.env.USE_POUCHDB === 'true' ? 'http://0.0.0.0' : 'http://couchdb',
-    db: 'chronicle',
-    dbPort: process.env.PORT || 5984,
-    auth: 'none',
-    logger: true,
-  };
+  env: process.env.NODE_ENV,
+  dbServer: process.env.USE_POUCHDB === 'true' ? 'http://0.0.0.0' : 'http://couchdb',
+  db: 'chronicle',
+  dbPort: process.env.PORT || 5984,
+  auth: 'none',
+  logger: true,
+};
