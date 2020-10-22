27a31d1480048d1f70e848521c71c3ff2e1a4b2a
Emel Alkim <emelalkim@msn.com>
2020-10-22 10:30:35 -0700

    fixes the updateviews issue

--- plugins/DIMSE.js
+++ plugins/DIMSE.js
@@ -47,7 +47,7 @@ async function dimse(fastify, options) {
           const filePath = path.join(__dirname, `../${options.tempDir}/${data.replace('file:', '')}`);
           fastify.dbPqueue
             .add(() => {
-              fastify.updateViews(dicomDB);
+              fastify.updateViews();
               fastify.log.info(`Saving DIMSE file ${filePath}`);
               return fastify.saveFile(filePath);
             })
